61cb51f0932cbf6478563dfe0d4951f34a771887
Javier Martínez <javiermartinezalvarez98@gmail.com>
2020-07-14 13:33:28 +0200

    Remove HTTP Header

--- api/header-banner.ts
+++ api/header-banner.ts
@@ -14,10 +14,7 @@ export default async function (req: NowRequest, res: NowResponse) {
     return res.status(200).end();
   }
 
-  res.setHeader("Content-Type", "image/svg+xml");
-  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
-
-  const array = decodeURIComponent(params.strings);
+  const array = JSON.parse(decodeURIComponent(params.strings));
   const font: FontConfig = {
     color: '#111',
     family: 'Helvetica Neue',
