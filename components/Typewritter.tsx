edc3d66c174d77d9e9020fe18e2ceaa57ed71a05
Javier Martínez <javiermartinezalvarez98@gmail.com>
2020-07-14 14:34:52 +0200

    Try another library

--- components/Typewritter.tsx
+++ components/Typewritter.tsx
@@ -1,6 +1,7 @@
 import { TextAlignProperty } from "csstype";
 import React from "react";
 import Typist from 'react-typist';
+const writer = require('typewriter-effect');
 
 export interface Props {
   strings: Array<string>;
@@ -17,22 +18,30 @@ export interface FontConfig {
 
 export const TextWriter: React.FC<Props> = ({ strings, font }) => {
   return (
-    <Typist>
-      { strings.map(text =>
-        <span
-          style={{
-            fontSize: font.size ? `${font.size }px` : undefined,
-            lineHeight: 1,
-            fontFamily: font.family,
-            color: font.color,
-            fontWeight: font.weight,
-            textAlign: font.align
-          }}
-        >
-          {text}
-        </span>
-      )}
-    </Typist>
+    // <Typist>
+    //   { strings.map(text =>
+    //     <span
+    //       style={{
+    //         fontSize: font.size ? `${font.size }px` : undefined,
+    //         lineHeight: 1,
+    //         fontFamily: font.family,
+    //         color: font.color,
+    //         fontWeight: font.weight,
+    //         textAlign: font.align
+    //       }}
+    //     >
+    //       {text}
+    //     </span>
+    //   )}
+    //   <span>hola</span>
+    // </Typist>
+    <writer.Typewriter
+      options={{
+        strings: strings,
+        autoStart: true,
+        loop: true,
+      }}
+    />
   );
 };
 
